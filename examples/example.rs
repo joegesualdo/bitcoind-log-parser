9adbf41f1137dcf242722290081491a3f1201fb8
Joe Gesualdo <joegesualdo@gmail.com>
2022-07-15 11:19:33 -0400

    Code cleanup

--- examples/example.rs
+++ examples/example.rs
@@ -5,15 +5,15 @@ use std::fs;
 use bitcoind_log_parser::types::*;
 
 fn main() {
-    let file_contents =
+    let file_contents: String =
         fs::read_to_string("../../../Desktop/bitcoind-log.txt").expect("failed to open file");
     let lines: Vec<&str> = file_contents.lines().collect();
 
     for line in lines {
-        let log_line = bitcoind_log_parser::parse_log_line(line);
+        let log_line: BitcoindLogLine = bitcoind_log_parser::parse_log_line(line);
         // Creating another copy just so I can print it out later without the compiler yelling at
         // me :(
-        let log_line2 = bitcoind_log_parser::parse_log_line(line);
+        let log_line2: BitcoindLogLine = bitcoind_log_parser::parse_log_line(line);
         match log_line.message_container.message {
             BitcoindLogMessage::NewOutboundPeerConnected(_) => {
                 println!("{:#?}", log_line2);
