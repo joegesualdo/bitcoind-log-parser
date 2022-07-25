da5060384cb29bc8eee4b009db26f1f23f8f6780
Joe Gesualdo <joegesualdo@gmail.com>
2022-07-25 10:07:47 -0400

    Add TODO

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,6 @@
 // Utilities ============
 // TODO: Implement failure handling
+// TODO: Should return a tuple, since we epect the type to always be (&str, &str)
 pub fn get_key_value_from_key_value_string(key_value_string: &str) -> Vec<&str> {
     // TODO: Use array here instead to improve performance
     let key_and_value: Vec<&str> = key_value_string.split("=").collect();
